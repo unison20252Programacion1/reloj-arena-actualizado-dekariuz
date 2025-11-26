6e477b191eef0bbccb027a284222a26aea7b7ecf
dekariuz <dekariuz@gmail.com>
2025-11-26 15:03:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # Completa las validaciones y llama a la función
 
 import sys
-from solucion import [NOMBRE_DE_LA_FUNCION]
-
+from solucion import reloj_arena
 def main():
     """
     data: lista de líneas leídas desde la entrada estándar o ingresadas por el usuario
@@ -28,13 +27,15 @@ def main():
     # Intentar convertir la altura a entero
     try:
         # TODO: Convertir m_str a entero y asignarlo a m
+        m=int(m_str)
         pass
     except ValueError:
         # TODO: imprimir "Error: La altura debe ser un numero entero" y salir
+        print("Error: La altura debe ser un numero entero")
         pass
 
     # TODO: llamar a la función triangulo_simetrico con los parámetros m y s
     # ERRATA: llamar a la función reloj_arena con los parámetros m y s.
-
+    reloj_arena(m, s[0])
 if __name__ == "__main__":
     main()
